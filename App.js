49d64885595e672c2f749a66f6f192f9c3ac7658
Francis Cerqueira <tauan_19@hotmail.com>
2022-01-25 13:31:03 -0300

    curso de React Native básico

---
+++ App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import TodoList from './components/TodoList';
+import Form from './components/Form';
+
+import { TodoService } from './services/TodoService';
+
+export default class App extends React.Component{
+    state = {
+      list: []
+    }
+  
+    async componentDidMount() {
+      const list = await TodoService.list();
+      this.setState({ list });
+    }
+  
+  add = async (text) => {
+    const newItem = await TodoService.create({ text });
+    const list = [...this.state.list, newItem];
+    this.setState({ list });
+      
+  }
+  remove = async (item) => {
+    await TodoService.remove(item.id);
+    const list = this.state.list.filter(itemList => itemList.id !== item.id);
+  }
+  
+  render(){
+  const { state } = this;
+  return (
+    
+    <View style={styles.container}>
+      <Form onAdd={this.add} />
+      <TodoList list={state.list} onRemove={this.remove}/>
+    </View>
+  );
+}
+}
+const styles = StyleSheet.create({
+  container: {
+    width: 85,
+    height: 60,
+    marginTop: 30,
+    backgroundColor: "#F18061",
+    borderWidth: 3,
+    borderColor: "#7B2B09",
+    borderTopStartRadius: 40,
+    borderTopEndRadius: 25,
+    borderBottomEndRadius: 40,
+    borderBottomStartRadius: 60,
+    
+  },
+});
---
+++ components/TodoList.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+import { View, Text, FlatList, Button } from "react-native";
+
+
+class TodoList extends Component{
+
+    static defaultProps = {
+        list: [],
+        onRemove: () =>{}
+    }
+    
+    handleRow = ({ item, index }) => {
+        return (
+            <View >
+                <Text >
+                    {this.formatListNumber(index)} - {item.text}
+                </Text>
+                <Button
+                    style={{width: 10}}
+                    title="X"
+                    color="#731919"
+                    onPress={()=> this.props.onRemove(item)}
+                />
+            </View>
+       )    
+    }
+
+    formatListNumber(number) {
+        const digits = 2;
+        return ('0'.repeat(digits) + (number +1)).slice(-digits)
+    }
+    
+    render() {
+        const { props } = this;
+        const keyExtractor = item => item.id;
+        return (
+            <View>
+                <FlatList
+                    data={props.list}
+                    keyExtractor={keyExtractor}
+                    renderItem={this.handleRow}
+                />
+            </View>
+        );
+    }
+}
+
+export default TodoList;
